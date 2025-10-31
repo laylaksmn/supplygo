e579ab37ba35a7462b87d6dd2c7c5ffcbcbaa5da
laylaksmn <layla.laksmana@gmail.com>
2025-10-31 15:21:34 +0700

    Update login DB

--- login.php
+++ login.php
@@ -2,53 +2,34 @@
 session_start();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $email = $_POST['email'];
-    $pass  = $_POST['password'];
+    include_once 'conn.php';
+    $email = addslashes(trim($_POST['email']));
+    $password = addslashes(trim($_POST['password']));
 
-    $fileUser = fopen('file.txt', 'r');
-    $users = [];
-
-    while (($line = fgets($fileUser)) !== false) {
-        $data = explode(',', trim($line));
-        if (count($data) === 3) {
-            $users[] = [
-                'nama' => trim($data[0]),
-                'email' => trim($data[1]),
-                'password' => trim($data[2])
-            ];
-        }
+    $stmt = $mysqli->prepare("SELECT password FROM user WHERE email=?");
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $getPassword = $result->fetch_object();
+    if ($password !== $getPassword->password){
+      header('Location: login.php?invalid=1');
+      die;
     }
-    fclose($fileUser);
-
-    $login = false;
-    for ($i = 0; $i < count($users); $i++) {
-        if ($users[$i]['email'] === $email && $users[$i]['password'] === $pass) {
-            $login = true;
-            $_SESSION['user'] = $users[$i]['email'];
-            $_SESSION['nama'] = $users[$i]['nama'];
-            break;
-        }
-    }
-
-    if ($login) {
-        header("Location: index.php");
-        exit();
-    } else {
-        $error = "Email atau password salah!";
-    }
+    $_SESSION['user'] = $email;
+    header("Location: dashboard.php");
+    die;
 }
 ?>
 <!DOCTYPE html>
 <html lang="id">
 <head>
   <meta charset="UTF-8">
-  <title>Halaman Login</title>
+  <title>Login - SUPPLYGO</title>
   <link rel="stylesheet" href="login.css">
 </head>
 <body>
   <div class="login-container">
     <h2><span class="orange">LOG</span> IN</h2>
-    <?php if(isset($error)) echo "<p class='error'>$error</p>"; ?>
     <form method="post" action="">
       <label>Email</label>
       <input type="email" name="email" required placeholder="Enter your email">
@@ -56,12 +37,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       <label>Password</label>
       <input type="password" name="password" required placeholder="Enter password">
 
-      <div class="options">
+      <!-- <div class="options">
         <label><input type="checkbox" name="remember"> Remember Me</label>
         <a href="#">Forgot Password?</a>
-      </div>
+      </div> -->
 
       <button type="submit" class="btn-login">LOG IN</button>
+      <?php if (isset($_GET['invalid'])): ?>
+      <p class="error">Email or password wrong</p>
+      <?php endif; ?>
 
       <p class="signup-text"> 
         Don't have an account? <a href="signup.php">Create an account</a>
